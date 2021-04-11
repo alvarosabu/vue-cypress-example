4e2115b71d3c7f22d32f014c73eb6a795b70f786
Alvaro Saburido <alvaro.saburido@gmail.com>
2021-04-11 12:28:26 +0200

    Youtube video example

--- src/components/awesome-card/AwesomeCard.spec.ts
+++ src/components/awesome-card/AwesomeCard.spec.ts
@@ -1,23 +1,69 @@
 import { mount } from "@cypress/vue";
 import AwesomeCard from "./AwesomeCard.vue";
+import { h } from "vue";
 
 describe("AwesomeCard", () => {
-  it("renders a message", () => {
-    const title = "This is the way!";
-    const media =
-      "https://images.unsplash.com/photo-1611250535839-94b88cf88bba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80";
-    const content =
-      "Arepa ipsum dolor amet parchita coroto Quisque eu le eche los perros amet cuál es la guachafita; Facilisis sifrino? Nam Suspendisse budare dejar el pelero regional light eu pretium que pasó el mío?. A hallaca soplar el bistec criolla amet! Edgar Rámirez tequeños ";
-    mount(AwesomeCard, {
-      propsData: {
-        title,
-        media,
-      },
-      slots: {
-        default: content,
-      },
+  context("Basic Rendering", () => {
+    it("renders a title", () => {
+      const title = "This is the way!";
+      const media =
+        "https://images.unsplash.com/photo-1611250535839-94b88cf88bba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80";
+      const content =
+        "Arepa ipsum dolor amet parchita coroto Quisque eu le eche los perros amet cuál es la guachafita; Facilisis sifrino? Nam Suspendisse budare dejar el pelero regional light eu pretium que pasó el mío?. A hallaca soplar el bistec criolla amet! Edgar Rámirez tequeños ";
+      mount(AwesomeCard, {
+        propsData: {
+          title,
+          media,
+        },
+        slots: {
+          default: h("p", content),
+        },
+      });
+
+      cy.get("h2").should("have.text", title);
     });
 
-    cy.get("h2").should("have.text", title);
+    it("renders a paragraph inside of the default slot", () => {
+      const title = "This is the way!";
+      const media =
+        "https://images.unsplash.com/photo-1611250535839-94b88cf88bba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80";
+      const content =
+        "Arepa ipsum dolor amet parchita coroto Quisque eu le eche los perros amet cuál es la guachafita; Facilisis sifrino? Nam Suspendisse budare dejar el pelero regional light eu pretium que pasó el mío?. A hallaca soplar el bistec criolla amet! Edgar Rámirez tequeños ";
+      mount(AwesomeCard, {
+        propsData: {
+          title,
+          media,
+        },
+        slots: {
+          default: h("p", content),
+        },
+      });
+
+      cy.get("p").should("have.text", content);
+    });
+  });
+
+  context("Actions", () => {
+    it("should hide the title when button is clicked", () => {
+      const title = "This is the way!";
+      const media =
+        "https://images.unsplash.com/photo-1611250535839-94b88cf88bba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80";
+      const content =
+        "Arepa ipsum dolor amet parchita coroto Quisque eu le eche los perros amet cuál es la guachafita; Facilisis sifrino? Nam Suspendisse budare dejar el pelero regional light eu pretium que pasó el mío?. A hallaca soplar el bistec criolla amet! Edgar Rámirez tequeños ";
+      mount(AwesomeCard, {
+        propsData: {
+          title,
+          media,
+        },
+        slots: {
+          default: h("p", content),
+        },
+      });
+
+      cy.get('[data-cy="card-button"]').click();
+
+      cy.get("h2").should("be.visible");
+      cy.get(".card-content").should("have.class", "text-green-400");
+    });
   });
 });
--- src/components/awesome-card/AwesomeCard.vue
+++ src/components/awesome-card/AwesomeCard.vue
@@ -6,15 +6,29 @@
       :src="media"
       alt="Card Image"
     />
-    <div class="card-content p-4 px-8 w-2/3 text-sm">
-      <h2 class="font-display text-lg mb-4 font-bold">{{ title }}</h2>
+    <div
+      class="card-content p-4 px-8 w-2/3 text-sm"
+      :class="{ 'text-green-400': clicked }"
+    >
+      <h2 class="font-display text-lg mb-4 font-bold" v-show="!clicked">
+        {{ title }}
+      </h2>
       <slot></slot>
+      <div class="py-4">
+        <button
+          class="btn px-2 py-1 bg-green-400 text-white text-sm rounded"
+          data-cy="card-button"
+          @click="handleClick"
+        >
+          Click me to see the magic
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 const props = {
   title: String,
@@ -23,8 +37,14 @@ const props = {
 export default defineComponent({
   name: "AwesomeCard",
   props,
-  setup(props) {
-    return {};
+  setup() {
+    const clicked = ref(false);
+
+    function handleClick() {
+      clicked.value = true;
+    }
+
+    return { clicked, handleClick };
   },
 });
 </script>
